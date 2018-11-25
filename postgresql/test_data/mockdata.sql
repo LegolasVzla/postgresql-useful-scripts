b17a3b8da896ac07f7811ca0317997ff6087d3e3
Manuel <manuel@ezgmz.com>
2018-11-24 23:57:31 -0400

    Creating tb_categories, data and foreign keys

--- postgresql/test_data/mockdata.sql
+++ postgresql/test_data/mockdata.sql
@@ -1,54 +1,67 @@
--- DROP DATABASE IF NOT EXISTS dev_postgres_database;
+-- DROP DATABASE IF EXISTS dev_postgres_database;
 CREATE DATABASE dev_postgres_database;
 
 DROP SCHEMA IF EXISTS temporal_schema CASCADE;
 CREATE SCHEMA temporal_schema;
 
+DROP TABLE IF EXISTS temporal_schema.tb_categories;
+CREATE TABLE temporal_schema.tb_categories (
+	id_categories SERIAL NOT NULL PRIMARY KEY,
+	name VARCHAR(255)
+);
+
 DROP TABLE IF EXISTS temporal_schema.tbl_endangered_animals;
 CREATE TABLE temporal_schema.tbl_endangered_animals (
-	id_endangered_animals SERIAL NOT NULL PRIMARY KEY,
-	name VARCHAR(255)
-)
-;
+	id_endangered_animals SERIAL NOT NULL,
+	name VARCHAR(255),
+	id_categories INTEGER,
+	PRIMARY KEY (id_endangered_animals,id_categories)
+);
 
-INSERT INTO temporal_schema.tbl_endangered_animals (name) VALUES ('Bengal tiger');
-INSERT INTO temporal_schema.tbl_endangered_animals (name) VALUES ('Giant panda');
-INSERT INTO temporal_schema.tbl_endangered_animals (name) VALUES ('Green Turtle');
-INSERT INTO temporal_schema.tbl_endangered_animals (name) VALUES ('Hippopotamus');
+ALTER TABLE temporal_schema.tbl_endangered_animals ADD CONSTRAINT tbl_endangered_animals_tb_categories
+	FOREIGN KEY (id_categories) REFERENCES temporal_schema.tb_categories (id_categories)
+ON UPDATE CASCADE;
 
 DROP TABLE IF EXISTS temporal_schema.tbl_reasons;
 CREATE TABLE temporal_schema.tbl_reasons (
 	id_reasons SERIAL NOT NULL PRIMARY KEY,
 	description VARCHAR(255)
-)
-;
+);
+
+DROP TABLE IF EXISTS temporal_schema.tbl_relation_endangered_reasons;
+CREATE TABLE temporal_schema.tbl_relation_endangered_reasons (
+	id_endangered_reasons_relation SERIAL,
+	id_endangered_animals INTEGER NOT NULL,
+	id_categories INTEGER NOT NULL,
+	id_reasons INTEGER NOT NULL,
+	PRIMARY KEY (id_endangered_reasons_relation,id_categories,id_endangered_animals,id_reasons)
+);
+
+ALTER TABLE temporal_schema.tbl_relation_endangered_reasons ADD CONSTRAINT tbl_relation_endangered_reasons_tbl_reasons
+	FOREIGN KEY (id_reasons) REFERENCES temporal_schema.tbl_reasons (id_reasons)
+ON UPDATE CASCADE;
+
+ALTER TABLE temporal_schema.tbl_relation_endangered_reasons ADD CONSTRAINT tbl_relation_endangered_reasons_tbl_endangered_animals
+	FOREIGN KEY (id_endangered_animals,id_categories) REFERENCES temporal_schema.tbl_endangered_animals (id_endangered_animals,id_categories)
+ON UPDATE CASCADE;
+
+
+INSERT INTO temporal_schema.tb_categories (name) VALUES ('Mammals');
+INSERT INTO temporal_schema.tb_categories (name) VALUES ('Reptile');
+
+INSERT INTO temporal_schema.tbl_endangered_animals (name,id_categories) VALUES ('Bengal tiger',1);
+INSERT INTO temporal_schema.tbl_endangered_animals (name,id_categories) VALUES ('Giant panda',1);
+INSERT INTO temporal_schema.tbl_endangered_animals (name,id_categories) VALUES ('Green Turtle',2);
+INSERT INTO temporal_schema.tbl_endangered_animals (name,id_categories) VALUES ('Hippopotamus',1);
 
 INSERT INTO temporal_schema.tbl_reasons (description) VALUES ('Poaching');
 INSERT INTO temporal_schema.tbl_reasons (description) VALUES ('Sales');
 INSERT INTO temporal_schema.tbl_reasons (description) VALUES ('Sea contamination');
 INSERT INTO temporal_schema.tbl_reasons (description) VALUES ('Destruction of territory');
 
-DROP TABLE IF EXISTS temporal_schema.tbl_relation_endangered_reasons;
-CREATE TABLE temporal_schema.tbl_relation_endangered_reasons (
-	id_endangered_reasons_relation SERIAL NOT NULL PRIMARY KEY,
-	id_endangered_animals INTEGER,
-	id_reasons INTEGER
-)
-;
-
-ALTER TABLE temporal_schema.tbl_relation_endangered_reasons add CONSTRAINT tbl_relation_endangered_reasons_tbl_endangered_animals
-	FOREIGN KEY (id_endangered_animals) REFERENCES temporal_schema.tbl_endangered_animals (id_endangered_animals)
-ON UPDATE CASCADE
-;
-
-ALTER TABLE temporal_schema.tbl_relation_endangered_reasons add CONSTRAINT tbl_relation_endangered_reasons_tbl_reasons
-	FOREIGN KEY (id_reasons) REFERENCES temporal_schema.tbl_reasons (id_reasons)
-ON UPDATE CASCADE
-;
-
-INSERT INTO temporal_schema.tbl_relation_endangered_reasons (id_endangered_animals,id_reasons) VALUES (1,1);
-INSERT INTO temporal_schema.tbl_relation_endangered_reasons (id_endangered_animals,id_reasons) VALUES (1,2);
-INSERT INTO temporal_schema.tbl_relation_endangered_reasons (id_endangered_animals,id_reasons) VALUES (2,1);
-INSERT INTO temporal_schema.tbl_relation_endangered_reasons (id_endangered_animals,id_reasons) VALUES (3,3);
-INSERT INTO temporal_schema.tbl_relation_endangered_reasons (id_endangered_animals,id_reasons) VALUES (4,1);
-INSERT INTO temporal_schema.tbl_relation_endangered_reasons (id_endangered_animals,id_reasons) VALUES (4,4);
+INSERT INTO temporal_schema.tbl_relation_endangered_reasons (id_endangered_animals,id_categories,id_reasons) VALUES (1,1,1);
+INSERT INTO temporal_schema.tbl_relation_endangered_reasons (id_endangered_animals,id_categories,id_reasons) VALUES (1,1,2);
+INSERT INTO temporal_schema.tbl_relation_endangered_reasons (id_endangered_animals,id_categories,id_reasons) VALUES (2,1,1);
+INSERT INTO temporal_schema.tbl_relation_endangered_reasons (id_endangered_animals,id_categories,id_reasons) VALUES (3,2,3);
+INSERT INTO temporal_schema.tbl_relation_endangered_reasons (id_endangered_animals,id_categories,id_reasons) VALUES (4,1,1);
+INSERT INTO temporal_schema.tbl_relation_endangered_reasons (id_endangered_animals,id_categories,id_reasons) VALUES (4,1,4);
