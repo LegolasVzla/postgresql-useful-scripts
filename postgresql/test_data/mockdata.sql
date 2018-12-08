566fbbc8756835dbf88d2fef6734b388e2009b05
Manuel <manuel2_m@hotmail.com>
2018-12-08 18:40:33 -0400

    Adding log tables for main tables

--- postgresql/test_data/mockdata.sql
+++ postgresql/test_data/mockdata.sql
@@ -4,14 +4,25 @@ CREATE DATABASE dev_postgres_database;
 DROP SCHEMA IF EXISTS temporal_schema CASCADE;
 CREATE SCHEMA temporal_schema;
 
-DROP TABLE IF EXISTS temporal_schema.tb_categories;
-CREATE TABLE temporal_schema.tb_categories (
+DROP TABLE IF EXISTS temporal_schema.tbl_categories;
+CREATE TABLE temporal_schema.tbl_categories (
 	id_categories SERIAL NOT NULL PRIMARY KEY,
 	name VARCHAR(255),
-	is_active boolean default true,
-	is_deleted boolean default false,
+	is_active BOOLEAN DEFAULT true,
+	is_deleted BOOLEAN DEFAULT false,
+    last_modified_date timestamp with time zone NOT NULL
+);
+
+DROP TABLE IF EXISTS temporal_schema.tb_log_categories;
+CREATE TABLE temporal_schema.tb_log_categories (
+	id_categories INTEGER,
+	name VARCHAR(255),
+	is_active BOOLEAN DEFAULT true,
+	is_deleted BOOLEAN DEFAULT false,
     last_modified_date timestamp with time zone NOT NULL,
-    created_date timestamp with time zone default NOW()
+	last_modified_by CHARACTER VARYING(100),
+	ip_user CHARACTER VARYING(100),
+	update_or_deleted_date timestamp with time zone NOT NULL
 );
 
 DROP TABLE IF EXISTS temporal_schema.tbl_endangered_animals;
@@ -19,25 +30,48 @@ CREATE TABLE temporal_schema.tbl_endangered_animals (
 	id_endangered_animals SERIAL NOT NULL,
 	name VARCHAR(255),
 	id_categories INTEGER,
-	is_active boolean default true,
-	is_deleted boolean default false,
+	is_active BOOLEAN DEFAULT true,
+	is_deleted BOOLEAN DEFAULT false,
     last_modified_date timestamp with time zone NOT NULL,
-    created_date timestamp with time zone default NOW(),
+    created_date timestamp with time zone DEFAULT NOW(),
 	PRIMARY KEY (id_endangered_animals,id_categories)
 );
 
-ALTER TABLE temporal_schema.tbl_endangered_animals ADD CONSTRAINT tbl_endangered_animals_tb_categories
-	FOREIGN KEY (id_categories) REFERENCES temporal_schema.tb_categories (id_categories)
+DROP TABLE IF EXISTS temporal_schema.tb_log_endangered_animals;
+CREATE TABLE temporal_schema.tb_log_endangered_animals (
+	id_endangered_animals INTEGER,
+	name VARCHAR(255),
+	is_active BOOLEAN DEFAULT true,
+	is_deleted BOOLEAN DEFAULT false,
+    last_modified_date timestamp with time zone NOT NULL,
+	last_modified_by CHARACTER VARYING(100),
+	ip_user CHARACTER VARYING(100),
+	update_or_deleted_date timestamp with time zone NOT NULL
+);
+
+ALTER TABLE temporal_schema.tbl_endangered_animals ADD CONSTRAINT tbll_endangered_animals_tb_categories
+	FOREIGN KEY (id_categories) REFERENCES temporal_schema.tbl_categories (id_categories)
 ON UPDATE CASCADE;
 
 DROP TABLE IF EXISTS temporal_schema.tbl_reasons;
 CREATE TABLE temporal_schema.tbl_reasons (
 	id_reasons SERIAL NOT NULL PRIMARY KEY,
 	description VARCHAR(255),
-	is_active boolean default true,
-	is_deleted boolean default false,
+	is_active BOOLEAN DEFAULT true,
+	is_deleted BOOLEAN DEFAULT false,
+    last_modified_date timestamp with time zone NOT NULL
+);
+
+DROP TABLE IF EXISTS temporal_schema.tb_log_reasons;
+CREATE TABLE temporal_schema.tb_log_reasons (
+	id_reasons INTEGER,
+	name VARCHAR(255),
+	is_active BOOLEAN DEFAULT true,
+	is_deleted BOOLEAN DEFAULT false,
     last_modified_date timestamp with time zone NOT NULL,
-    created_date timestamp with time zone default NOW()
+	last_modified_by CHARACTER VARYING(100),
+	ip_user CHARACTER VARYING(100),
+	update_or_deleted_date timestamp with time zone NOT NULL
 );
 
 DROP TABLE IF EXISTS temporal_schema.tbl_relation_endangered_reasons;
@@ -46,11 +80,23 @@ CREATE TABLE temporal_schema.tbl_relation_endangered_reasons (
 	id_endangered_animals INTEGER NOT NULL,
 	id_categories INTEGER NOT NULL,
 	id_reasons INTEGER NOT NULL,
-	is_active boolean default true,
-	is_deleted boolean default false,
+	is_active BOOLEAN DEFAULT true,
+	is_deleted BOOLEAN DEFAULT false,
     last_modified_date timestamp with time zone NOT NULL,
-    created_date timestamp with time zone default NOW(),
+    created_date timestamp with time zone DEFAULT NOW(),
 	PRIMARY KEY (id_endangered_reasons_relation,id_categories,id_endangered_animals,id_reasons)
+);
+
+DROP TABLE IF EXISTS temporal_schema.tb_log_relation_endangered_reasons;
+CREATE TABLE temporal_schema.tb_log_relation_endangered_reasons (
+	id_relation_endangered_reasons INTEGER,
+	name VARCHAR(255),
+	is_active BOOLEAN DEFAULT true,
+	is_deleted BOOLEAN DEFAULT false,
+    last_modified_date timestamp with time zone NOT NULL,
+	last_modified_by CHARACTER VARYING(100),
+	ip_user CHARACTER VARYING(100),
+	update_or_deleted_date timestamp with time zone NOT NULL
 );
 
 ALTER TABLE temporal_schema.tbl_relation_endangered_reasons ADD CONSTRAINT tbl_relation_endangered_reasons_tbl_reasons
@@ -61,9 +107,8 @@ ALTER TABLE temporal_schema.tbl_relation_endangered_reasons ADD CONSTRAINT tbl_r
 	FOREIGN KEY (id_endangered_animals,id_categories) REFERENCES temporal_schema.tbl_endangered_animals (id_endangered_animals,id_categories)
 ON UPDATE CASCADE;
 
-
-INSERT INTO temporal_schema.tb_categories (name,last_modified_date) VALUES ('Mammals',NOW());
-INSERT INTO temporal_schema.tb_categories (name,last_modified_date) VALUES ('Reptile',NOW());
+INSERT INTO temporal_schema.tbl_categories (name,last_modified_date) VALUES ('Mammals',NOW());
+INSERT INTO temporal_schema.tbl_categories (name,last_modified_date) VALUES ('Reptile',NOW());
 
 INSERT INTO temporal_schema.tbl_endangered_animals (name,id_categories,last_modified_date) VALUES ('Bengal tiger',1,NOW());
 INSERT INTO temporal_schema.tbl_endangered_animals (name,id_categories,last_modified_date) VALUES ('Giant panda',1,NOW());
