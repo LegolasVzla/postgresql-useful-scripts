4198e8d62d0f07de7e90955330557e1fb6904146
Manuel Carrero <mcomercio.web@gmail.com>
2019-07-09 22:57:43 -0400

    Execute all modified

--- execute_all.sh
+++ execute_all.sh
@@ -1,6 +1,5 @@
 psql -U postgres -a -f ./postgresql/main_database.sql
-psql -U postgres -d dev_postgres_database -a -c "CREATE SCHEMA temporal_schema;"
-psql -U postgres -d dev_postgres_database -a -f ./postgresql/dev_postgres_database/temporal_schema/functions/udf_kill_connections_pg3.sql
-psql -U postgres -d dev_postgres_database -a -f ./postgresql/dev_postgres_database/temporal_schema/functions/udf_kill_connections_pg4.sql
-psql -U postgres -d dev_postgres_database -a -f ./postgresql/dev_postgres_database/temporal_schema/functions/udf_check_value.sql
-psql -U postgres -d dev_postgres_database -a -f ./postgresql/dev_postgres_database/temporal_schema/functions/udf_adjust_sequence.sql
+psql -U postgres -d dev_postgres_database -a -f ./postgresql/dev_postgres_database/public/functions/udf_kill_connections_pg3.sql
+psql -U postgres -d dev_postgres_database -a -f ./postgresql/dev_postgres_database/public/functions/udf_kill_connections_pg4.sql
+psql -U postgres -d dev_postgres_database -a -f ./postgresql/dev_postgres_database/public/functions/udf_check_value.sql
+psql -U postgres -d dev_postgres_database -a -f ./postgresql/dev_postgres_database/public/functions/udf_adjust_sequence.sql
