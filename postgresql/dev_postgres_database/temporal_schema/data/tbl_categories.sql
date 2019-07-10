07097610834e82e81e45ee0bca772b16d70d51f6
Manuel Carrero <mcomercio.web@gmail.com>
2019-07-09 22:28:27 -0400

    Adding call to some sql functions in execute_all.sh file

--- postgresql/dev_postgres_database/temporal_schema/data/tbl_categories.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO temporal_schema.tbl_categories (name,last_modified_date) VALUES ('Mammals',NOW());
-INSERT INTO temporal_schema.tbl_categories (name,last_modified_date) VALUES ('Reptile',NOW());
